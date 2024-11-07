be35f577cc70dac7f0dac3b630ce3dd2c4765d22
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2024-11-07 10:36:43 +0100

    slutuppgift 1

--- näsan1/Program.cs
+++ näsan1/Program.cs
@@ -1,15 +1,48 @@
-﻿for (int i = 0; i < 5; i++)
+﻿// for (int i = 0; i < 5; i++)
+// {
+//     string num = Console.ReadLine();
+//     int numInt;
+//     bool valid = int.TryParse(num, out numInt);
+//     while (valid == false)
+//     {
+//         System.Console.WriteLine("Nej, igen.");
+//         num = Console.ReadLine();
+//         valid = int.TryParse(num, out numInt);
+//     }
+
+//     if (numInt > 5) System.Console.WriteLine("Högre än 5!");
+//     else System.Console.WriteLine(":(");
+// }
+
+int secretNumber = Random.Shared.Next(100);
+
+while (true)
 {
-    string num = Console.ReadLine();
-    int numInt;
-    bool valid = int.TryParse(num, out numInt);
+    System.Console.WriteLine("Guess a number between 0-100\n");
+    string guess = Console.ReadLine();
+    int guessedNumber = 0;
+    bool valid = int.TryParse(guess, out guessedNumber);
+
     while (valid == false)
     {
-        System.Console.WriteLine("Nej, igen.");
-        num = Console.ReadLine();
-        valid = int.TryParse(num, out numInt);
+        System.Console.WriteLine("\nNo. A number between 0-100");
+        guess = Console.ReadLine();
+        valid = int.TryParse(guess, out guessedNumber);
     }
 
-    if (numInt > 5) System.Console.WriteLine("Högre än 5!");
-    else System.Console.WriteLine(":(");
+    if (guessedNumber < secretNumber)
+    {
+        System.Console.WriteLine("Too low. Try again");
+    }
+    else if (guessedNumber > secretNumber)
+    {
+        System.Console.WriteLine("Too high. Try again");
+    }
+    else if (guessedNumber == secretNumber)
+    {
+        System.Console.WriteLine("Correct!");
+        break;
+    }
 }
+
+Console.ReadLine();
